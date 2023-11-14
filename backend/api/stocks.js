522f628b229754c426f9ea7436265742391dcd27
ashok <ashokpaila7013@gmail.com>
2023-11-15 01:19:01 +0530

    second

--- backend/api/stocks.js
+++ backend/api/stocks.js
@@ -1,12 +1,7 @@
 
-// Mock API  to Get Random Stock Prices
-module.exports =async (req, res) => {
-
-    const express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const app = express();
-
 
 app.use(express.json());
 
@@ -18,9 +13,13 @@ const stockSchema = new mongoose.Schema({
   name: String,
   price: Number,
 });
+
 // Creating Stock Model
 const Stock = mongoose.model('Stock', stockSchema);
 
+// Mock API  to Get Random Stock Prices
+module.exports =async (req, res) => {
+    
   try {
     const stocks = await Stock.find();
     const updatedStocks = stocks.map((stock) => ({
