bd18fec9281cc151e57e10adc2ba261efcc9cacf
ashok <ashokpaila7013@gmail.com>
2023-11-15 01:59:55 +0530

    secon

--- backend/api/stocks.js
+++ backend/api/stocks.js
@@ -5,6 +5,31 @@ const app = express();
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+  const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
+  
+  module.exports = allowCors(handler)
+  
+
 // Connecting to MongoDB 
 mongoose.connect("mongodb+srv://ashok7013:ashok7013@cluster0.v5nbxez.mongodb.net/?retryWrites=true&w=majority");
 
@@ -32,3 +57,4 @@ module.exports =async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 }
+
