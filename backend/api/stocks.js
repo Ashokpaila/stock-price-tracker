1de68171b105f6f5d24a23aeb90c245c4143544c
ashok <ashokpaila7013@gmail.com>
2023-11-15 00:51:52 +0530

    secon

--- backend/api/stocks.js
+++ backend/api/stocks.js
@@ -1,5 +1,36 @@
 
-module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello from the stocks API!' });
-  };
-  
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+// Connecting to MongoDB 
+mongoose.connect("mongodb+srv://ashok7013:ashok7013@cluster0.v5nbxez.mongodb.net/?retryWrites=true&w=majority");
+
+// Defining Stock Schema
+const stockSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+});
+
+// Creating Stock Model
+const Stock = mongoose.model('Stock', stockSchema);
+
+// Mock API  to Get Random Stock Prices
+app.get('/apis/stocks', async (req, res) => {
+  try {
+    const stocks = await Stock.find();
+    const updatedStocks = stocks.map((stock) => ({
+      name: stock.name,
+      price: stock.price + Math.random() * 10 - 5, //generating random price
+    }));
+    res.json(updatedStocks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
